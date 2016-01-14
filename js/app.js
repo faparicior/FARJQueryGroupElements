6b603bc6720d6c16c7c4f505a57625c248482aac
Fernando Aparicio <faparicior@gmail.com>
2016-01-14 17:52:48 +0100

    Redefine JQuery selectors

--- js/app.js
+++ js/app.js
@@ -2,25 +2,24 @@
     'use strict';
 
     var defaults = {
-            'version'              : '1.0',
+            'version'             : '1.0',
+            'id_container'        : 'demo',
             'template' : { // HTML segments
-                'element_master'  : '<span class="display" id="newElementMaster" contenteditable="true">Add new</span>',
-                'element'         : '<span class="display" id="newElement" contenteditable="true">Add new</span>',
-                'element_free'    : '<span class="display" id="newFreeElement" contenteditable="true">Add new</span>',
-                'edit_element'    : '<span class="fa fa-pencil display-edit "></span>',
+                'new_element'     : '<span class="display" contenteditable="true">Add new</span>',
+                'edit_element'    : '<span class="fa fa-pencil display-edit"></span>',
                 'close_element'   : '<span class="fa fa-times display-close close-element"></span>',
-                'assign_element'  : '<span class="fa fa-tag display-tag "></span>'
+                'assign_element'  : '<span class="fa fa-tag display-tag"></span>'
             },
             'demo_values': JSON.parse(
-                '{ "group_categories" : [' +
+                '{ "far_master_elements" : [' +
                 '{ "id":"1" , "desc":"Alimentacion" },' +
                 '{ "id":"4" , "desc":"Servicios profesionales" },' +
                 '{ "id":"5" , "desc":"Salud" } ],' +
-                '  "free_categories" : [' +
+                '  "far_free_elements" : [' +
                 '{ "id":"7" , "desc":"Farmacia" },' +
                 '{ "id":"8" , "desc":"Optica" },' +
                 '{ "id":"9" , "desc":"Parafarmacia" } ],' +
-                '  "categories" : [' +
+                '  "far_elements" : [' +
                 '{ "id":"1" , "desc":"Ferreteria", "relId": "1" },' +
                 '{ "id":"4" , "desc":"Supermercado", "relId": "1" },' +
                 '{ "id":"5" , "desc":"Bar / Restaurante", "relId": "4" } ]}'
@@ -31,85 +30,85 @@
     methods = {
         init : function (opts) {
             return this.each(function () {
+
                 if ($.isEmptyObject(settings)) {
                     settings = $.extend(true, defaults, opts);
-
                     // non configurable settings
-                    settings.document = window.document;
-                    settings.$document = $(settings.document);
-                    settings.$window = $(window);
                     settings.$content_el = $(this);
                 }
-                methods.createElements(settings.demo_values['group_categories'], 'group_categories', 1);
-                methods.createElements(settings.demo_values['categories'], 'categories', 2);
-                methods.createElements(settings.demo_values['free_categories'], 'free_categories', 3);
+
+                methods.createElements(settings.demo_values['far_master_elements'], 'far_master_elements', 1);
+                methods.createElements(settings.demo_values['far_elements'], 'far_elements', 2);
+                methods.createElements(settings.demo_values['far_free_elements'], 'far_free_elements', 3);
+
+                settings.ulWatcher = settings.$content_el.find('ul');
+                settings.masterWatcher = settings.ulWatcher.filter('.far_master_elements');
+                settings.elementsWatcher = settings.ulWatcher.filter('.far_elements');
+                settings.freeElementsWatcher = settings.ulWatcher.filter('.far_free_elements');
 
                 //
                 // Event methods
                 //
-                var ulWatcher = jQuery('ul');
-                var groupWatcher = jQuery('#group_categories');
-                var categoriesWatcher = jQuery('#categories');
-                var freeCategoriesWatcher = jQuery('#free_categories');
-
                 // Enter Key disable edit
-                ulWatcher.on('keypress', '.display', function (e) {
+                settings.ulWatcher.on('keypress', '.display', function (e) {
                     return e.which != 13;
                 });
-
                 // Selected element
-                groupWatcher.on('click', 'li' , methods.groupSelected);
-                categoriesWatcher.on('click', 'li' , methods.categoriesSelected);
-                freeCategoriesWatcher.on('click', 'li' , methods.freeCategoriesSelected);
-
+                settings.ulWatcher.on('click', 'li' , methods.masterSelected);
+                settings.elementsWatcher.on('click', 'li' , methods.elementsSelected);
+                settings.freeElementsWatcher.on('click', 'li' , methods.freeElementsSelected);
                 // Select child categories from master
-                groupWatcher.on('click focus', '.display', methods.selectChildCategories);
-
+                settings.masterWatcher.on('click focus', 'li', methods.selectChildCategories);
                 // Edit element
-                ulWatcher.on('click', '.display-edit', methods.editElement);
-                ulWatcher.on('focusout', '.display', methods.uneditElement);
+                settings.ulWatcher.on('click', '.display-edit', methods.editElement);
+////                settings.ulWatcher.on('focusout', '.display', methods.uneditElement);
                 // New element Master
-                ulWatcher.on('focus', '#newElementMaster', methods.newElementMaster);
+                settings.masterWatcher.on('click', '.newElementMaster', methods.newElementMaster);
                 // New element
-                ulWatcher.on('focus', '#newElement', methods.newElement);
+                settings.elementsWatcher.on('click', '.newElement', methods.newElement);
                 // New free element
-                ulWatcher.on('focus', '#newFreeElement', methods.newFreeElement);
+                settings.freeElementsWatcher.on('click', '.newElementFree', methods.newFreeElement);
                 // Tagged free element
-                ulWatcher.on('click', '.display-tag', methods.assignElement);
+                settings.freeElementsWatcher.on('click', '.display-tag', methods.assignElement);
                 // Remove group categories
-                groupWatcher.on('click', '.close-element', methods.removeMasterElement);
+                settings.masterWatcher.on('click', '.close-element', methods.removeMasterElement);
                 // Remove element
-                categoriesWatcher.on('click', '.close-element', methods.removeElement);
+                settings.elementsWatcher.on('click', '.close-element', methods.removeElement);
                 // Remove free categories
-                freeCategoriesWatcher.on('click', '.close-element', methods.removeFreeElement);
+                settings.freeElementsWatcher.on('click', '.close-element', methods.removeFreeElement);
             });
         },
-        groupSelected: function () {
-            var id = $(this).data('uid');
-            var spanCategories = $('#categories li span');
-            console.log(id);
-            $(this).closest('ul').attr('data-id-selected', id);
-            spanCategories.filter('[data-relid = "' + id + '"]').closest('li').show();
-            spanCategories.filter('[id = "newElement"]').closest('li').show();
-            $(this).closest('ul').find('.elementSelected').removeClass('elementSelected');
-            $(this).addClass('elementSelected');
+        masterSelected: function () {
+            var li = $(this);
+            var span = li.find('span');
+            var id = span.data('uid');
+
+            settings.masterWatcher.attr('data-id-selected', id);
+            span.filter('[data-relid = "' + id + '"]').closest('li').show();
+            span.filter('[id = "newElement"]').closest('li').show();
+            li.closest('ul').find('.elementSelected').removeClass('elementSelected');
+            li.addClass('elementSelected');
         },
-        categoriesSelected: function () {
-            $(this).closest('ul').find('.elementSelected').removeClass('elementSelected');
-            $(this).addClass('elementSelected');
+        elementsSelected: function () {
+            var li = $(this);
+
+            li.closest('ul').find('.elementSelected').removeClass('elementSelected');
+            li.addClass('elementSelected');
         },
-        freeCategoriesSelected: function() {
-            $(this).closest('ul').find('.elementSelected').removeClass('elementSelected');
-            $(this).addClass('elementSelected');
+        freeElementsSelected: function() {
+            var li = $(this);
+
+            li.closest('ul').find('.elementSelected').removeClass('elementSelected');
+            li.addClass('elementSelected');
         },
         selectChildCategories: function() {
             var id = $(this).data('uid');
-            var spanCategories = $('#categories li span');
+            var ul_master = $(this).closest('ul');
+            var li_child = settings.elementsWatcher.find('li');
 
-            $(this).closest('ul').attr('data-id-selected', id);
-            spanCategories.closest('li').hide();
-            spanCategories.filter('[data-relid = "' + id + '"]').closest('li').show();
-            spanCategories.filter('[id = "newElement"]').closest('li').show();
+            ul_master.attr('data-id-selected', id);
+            li_child.not('[data-relid = "' + id + '"]').not('.newElement').hide();
+            li_child.filter('[data-relid = "' + id + '"]').show();
         },
         editElement: function () {
             var element = $(this).closest('li').find('.display');
@@ -125,9 +124,9 @@
             var element = $(this).closest('li').detach();
             element.find('.display-tag').remove();
 
-            var idMaster = $('#group_categories').attr('data-id-selected');
-            element.find('.display').attr('data-relid', idMaster);
-            $('#categories').find('#newElement').closest('li').before(element);
+            var idMaster = settings.masterWatcher.attr('data-id-selected');
+            element.attr('data-relid', idMaster);
+            settings.elementsWatcher.find('.newElement').closest('li').before(element);
         },
         newElementMaster: function () {
             var newElement = methods.template_li_new_item(1);
@@ -135,15 +134,19 @@
             var newClose = methods.template_close_element();
             var uid_data = methods.generateUUID();
 
-            $(this).closest('li').find('.display-close ').addClass('close-element');
-            $(this).removeAttr('id');
-            $(this).attr('data-uid',uid_data);
-            $(this).closest('ul').attr('data-id-selected', uid_data);
+            var li = $(this).closest('li');
+            var ul = $(this).closest('ul');
+            var span = $(this).find('.display');
 
-            $(this).after(newClose);
-            $(this).after(newEdit);
-            methods.selectText($(this));
-            $(this).closest('li').after(newElement);
+//            li.find('.display-close ').addClass('close-element');
+            li.removeClass('newElementMaster');
+            li.attr('data-uid',uid_data);
+            ul.attr('data-id-selected', uid_data);
+
+            span.after(newClose);
+            span.after(newEdit);
+            methods.selectText(span);
+            li.after(newElement);
         },
         newElement: function () {
             var newElement = methods.template_li_new_item(2);
@@ -151,17 +154,19 @@
             var newClose = methods.template_close_element();
             var uid_data = methods.generateUUID();
 
-            $(this).closest('li').find('.display-close ').addClass('close-element');
-            $(this).removeAttr('id');
+            var li = $(this).closest('li');
+            var span = $(this).find('.display');
+
+            li.removeClass('newElement');
             $(this).attr('data-uid',uid_data);
 
-            var idMaster = $('#group_categories').attr('data-id-selected');
-            $(this).attr('data-relid',idMaster);
+            var idMaster = settings.masterWatcher.attr('data-id-selected');
+            li.attr('data-relid', idMaster);
 
-            $(this).after(newClose);
-            $(this).after(newEdit);
-            methods.selectText($(this));
-            $(this).closest('li').after(newElement);
+            span.after(newClose);
+            span.after(newEdit);
+            methods.selectText(span);
+            span.closest('li').after(newElement);
         },
         newFreeElement: function () {
             var newElement = methods.template_li_new_item(3);
@@ -170,15 +175,18 @@
             var uid_data = methods.generateUUID();
             var newAssign = methods.template_assign_element();
 
-            $(this).closest('li').find('.display-close').addClass('close-element');
-            $(this).removeAttr('id');
+            var li = $(this).closest('li');
+            var span = $(this).find('.display');
+
+            //li.find('.display-close').addClass('close-element');
+            li.removeClass('newElementFree');
             $(this).attr('data-uid',uid_data);
 
-            $(this).after(newClose);
-            $(this).after(newEdit);
-            $(this).after(newAssign);
-            methods.selectText($(this));
-            $(this).closest('li').after(newElement);
+            span.after(newClose);
+            span.after(newEdit);
+            span.after(newAssign);
+            methods.selectText(span);
+            li.after(newElement);
         },
         removeElement: function () {
             var newAssign = methods.template_assign_element();
@@ -186,7 +194,8 @@
 
             element.find('.display').removeAttr('data-relid');
             element.find('.display').before(newAssign);
-            $('#free_categories').find('#newFreeElement').closest('li').before(element);
+            settings.freeElementsWatcher.find('.newElementFree').closest('li').before(element);
+            //$('#free_categories').find('#newFreeElement').closest('li').before(element);
         },
         removeFreeElement: function () {
             $(this).closest('li').remove();
@@ -195,15 +204,40 @@
             // TODO: Delete all dependencies
             var id = $(this).closest('li').find('.display').attr('data-uid');
             var rels = $('#categories').find('[data-relid = "' + id + '"]');
-            console.log(rels);
-            // TODO-END: Delete all dependencies
+            var rels_li = rels.closest('li');
 
-//            $(this).closest('li').remove();
+            rels.removeAttr('data-relid');
+            rels_li.prepend(methods.template_assign_element());
+            rels_li.detach();
+            $('#free_categories').find('#newFreeElement').closest('li').before(rels_li);
+            $(this).closest('li').remove();
+            // TODO: Discover how hide new element
+            $('#categories').find('#newElement').closest('li').hide();
+            // TODO: Discover how hide new element
         },
         template_li_master: function (values) {
             return $(
-                '<li>' +
-                    '<span class="display" data-uid="' + values.id + '" contenteditable="false">' + values.desc + '</span>' +
+                '<li data-uid="' + values.id + '">' +
+                    '<span class="display" " contenteditable="false">' + values.desc + '</span>' +
+                    settings.template.edit_element +
+                    settings.template.close_element +
+                '</li>'
+            );
+        },
+        template_li: function (values) {
+            return $(
+                '<li data-uid="' + values.id + '" data-relid="' + values.relId + '">' +
+                '<span class="display" contenteditable="false">' + values.desc + '</span>' +
+                settings.template.edit_element +
+                settings.template.close_element +
+                '</li>'
+            );
+        },
+        template_li_free: function (values) {
+            return $(
+                '<li data-uid="' + values.id + '">' +
+                    settings.template.assign_element +
+                    '<span class="display" contenteditable="false">' + values.desc + '</span>' +
                     settings.template.edit_element +
                     settings.template.close_element +
                 '</li>'
@@ -224,49 +258,30 @@
                 settings.template.assign_element
             );
         },
-        template_li: function (values) {
-            return $(
-                '<li>' +
-                    '<span class="display" data-uid="' + values.id + '" data-relid="' + values.relId + '" contenteditable="false">' + values.desc + '</span>' +
-                    settings.template.edit_element +
-                    settings.template.close_element +
-                '</li>'
-            );
-        },
-        template_li_free: function (values) {
-            return $(
-                '<li>' +
-                settings.template.assign_element +
-                '<span class="display" data-uid="' + values.id + '" data-relid="' + values.relId + '" contenteditable="false">' + values.desc + '</span>' +
-                settings.template.edit_element +
-                settings.template.close_element +
-                '</li>'
-            );
-        },
         template_li_new_item: function (elementType) {
             if(elementType == 1) {
                 return $(
-                    '<li>' +
-                    settings.template.element_master +
+                    '<li class="newElementMaster">' +
+                    settings.template.new_element +
                     '</li>'
                 );
             } else if(elementType == 2) {
                 return $(
-                    '<li>' +
-                    settings.template.element +
+                    '<li class="newElement">' +
+                    settings.template.new_element +
                     '</li>'
                 );
             } else {
                 return $(
-                    '<li>' +
-                    settings.template.element_free +
+                    '<li class="newElementFree">' +
+                    settings.template.new_element +
                     '</li>'
                 );
             }
         },
         template_ul: function (idUl) {
             return $(
-                '<ul id="' + idUl + '">' +
+                '<ul class="' + idUl + '">' +
                 '</<ul>'
             );
         },
@@ -275,14 +290,14 @@
 
             $.each(obj, function (i, val) {
                 if(listType == 1) {
-                    methods.template_li_master(val).appendTo('#' + idUl);
+                    methods.template_li_master(val).appendTo('.' + idUl);
                 } else if(listType == 2) {
-                    methods.template_li(val).appendTo('#' + idUl);
+                    methods.template_li(val).appendTo('.' + idUl);
                 } else {
-                    methods.template_li_free(val).appendTo('#' + idUl);
+                    methods.template_li_free(val).appendTo('.' + idUl);
                 }
             });
-            methods.template_li_new_item(listType).appendTo('#' + idUl);
+            methods.template_li_new_item(listType).appendTo('.' + idUl);
         },
         generateUUID: function () {
             var d = new Date().getTime();
